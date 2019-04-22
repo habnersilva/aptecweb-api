ce967f62db5ca184224f86f047a4eaaea0c37a00
Habner Silva <habner@aptec.com.br>
2019-04-22 13:08:25 -0300

    Primeira versão

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const ejs = require('ejs')
+
+// Carrega dados de dados.json
+const data = JSON.parse(fs.readFileSync("dados.json"))
+
+
+ejs.renderFile('templates/index.ejs', data, function(err, str){
+  
+    if(err)
+        throw err
+
+    // Gravar em Novo arquivo index.html
+    fs.writeFile("./index.html", str, (err) => {
+    
+        if(err)
+            throw err
+
+        console.log('Arquivo Html gerado')
+
+    })
+
+})
